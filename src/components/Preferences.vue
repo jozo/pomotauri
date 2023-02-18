80fd459d35d0e4f38efd266c01c79aac9a3157ab
jozo <hi@jozo.io>
2023-02-18 16:16:38 +0000

    #4 - Add preferences, upgrade packages, change sound notification, change CI to ubuntu 22.04 (#7)

---
+++ src/components/Preferences.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="row">
+      <input type="checkbox" id="sounds" :checked="playSound"
+             @click="$emit('update:playSound', $event.target.checked)"
+      >
+      <label for="sounds">Play notification sound</label>
+    </div>
+    <div class="row">
+      <div>
+        <input type="number" min="1" :value="durationWork"
+               @input="$emit('update:durationWork', $event.target.value)">
+      </div>
+      <div>(mins) Work step</div>
+    </div>
+    <div class="row">
+      <div>
+        <input type="number" min="1" :value="durationBreak"
+               @input="$emit('update:durationBreak', $event.target.value)">
+      </div>
+      <div>(mins) Break</div>
+    </div>
+    <div class="row">
+      <div>
+        <input type="number" min="1" :value="durationLongBreak"
+               @input="$emit('update:durationLongBreak', $event.target.value)">
+      </div>
+      <div>(mins) Long break</div>
+    </div>
+  </div>
+  <div class="buttons">
+    <button @click="$emit('save')">Save</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Preferences",
+  props: {
+    playSound: Boolean,
+    durationWork: Number,
+    durationBreak: Number,
+    durationLongBreak: Number,
+  },
+  emits: ["update:playSound", "update:durationWork",
+    "update:durationBreak", "update:durationLongBreak", "save"]
+}
+</script>
+
+<style scoped>
+.row {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 10px;
+}
+
+input[type=number] {
+  width: 50px;
+  display: block;
+  margin-right: 10px;
+  font-weight: bold;
+}
+</style>
--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,125 +1,134 @@
 <template>
-<div>
-    <div id="timer">{{ display }}</div>
-    <div>
-        <button v-if="intervalId" @click="stopTimer" class="btn-timer">
-            Stop timer
-        </button>
-        <button v-else @click="startTimer" class="btn-timer">
-            Start timer
-        </button>
-        <button @click="goToNextStep" class="btn-timer" title="Next step">
-            >
-        </button>
-    </div>
-</div>
+  <div id="timer">{{ display }}</div>
+  <div class="buttons">
+    <button @click="toggleTimer" class="btn-timer" title="Start/Stop">
+      <span v-if="intervalId">⏹</span>
+      <span v-else>⏵</span>
+    </button>
+    <button @click="goToNextStep" class="btn-timer" title="Next step">⏭</button>
+    <button class="btn-timer" title="Preferences"
+            @click="togglePreferences">
+      ⛭
+    </button>
+  </div>
 </template>
 
 <script>
 class Step {
-    constructor(name, duration) {
-        this.name = name
-        this.duration = duration
-    }
+  constructor(name, duration) {
+    this.name = name
+    this.duration = duration
+  }
 }
 
-
 class Schedule {
-    constructor() {
-        this.steps = [
-            new Step("work", 25*60),
-            new Step("break", 5*60),
-        ]
-        this.stepIndex = 0
-    }
-    
-    currentStep() {
-        const idx = this.stepIndex % this.steps.length
-        return this.steps[idx]
-    }
-    
-    moveToNextStep() {
-        this.stepIndex++
-    }
+  constructor(workMins, breakMins, longBreakMins) {
+    const workStep = new Step("work", workMins * 60)
+    const breakStep = new Step("break", breakMins * 60)
+
+    this.steps = [
+      workStep, breakStep, workStep, breakStep, workStep, breakStep,
+      new Step("break", longBreakMins * 60),
+    ]
+    this.stepIndex = 0
+  }
+
+  currentStep() {
+    const idx = this.stepIndex % this.steps.length
+    return this.steps[idx]
+  }
+
+  moveToNextStep() {
+    this.stepIndex++
+  }
 }
 
 export default {
-    data() {
-        const schedule = new Schedule()
-        return {
-            intervalId: undefined,
-            startDate: undefined,
-            schedule: schedule,
-            display: this.formatTimeSpan(schedule.currentStep().duration),
-        }
-    },
-    methods: {
-        startTimer: function () {
-            this.startDate = new Date()
-
-            this.intervalId = setInterval(() => {
-                const leftSeconds = this.secondsLeftInStep()
-                this.setTimerDisplay(this.formatTimeSpan(leftSeconds))
-
-                if (leftSeconds <= 0) {
-                    this.schedule.moveToNextStep()
-                    this.stopTimer()
-                    this.notify()
-                }
-            }, 100)
-        },
-        stopTimer: function () {
-            clearInterval(this.intervalId)
-            this.intervalId = null
-            this.setTimerDisplay(this.formatTimeSpan(this.schedule.currentStep().duration))
-        },
-        goToNextStep: function () {
-            this.schedule.moveToNextStep()
-            this.stopTimer()
-        },
-        notify: function () {
-            let audio = document.getElementById('beepAudio');
-            audio.play()
-        },
-        secondsLeftInStep: function () {
-            const diffSeconds = (new Date() - this.startDate) / 1000
-            return this.schedule.currentStep().duration - diffSeconds
-        },
-        formatTimeSpan: function (spanSeconds) {
-            const minutes = Math.floor(spanSeconds / 60)
-            const seconds = Math.floor(spanSeconds - minutes * 60)
-            return this.doubleDigit(minutes) + ":" + this.doubleDigit(seconds)
-        },
-        doubleDigit: function (number) {
-            return (number).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
-        },
-        setTimerDisplay: function (text) {
-            this.display = text
-            document.title = text
-        }
+  props: {
+    durationWork: Number,
+    durationBreak: Number,
+    durationLongBreak: Number,
+    displayPreferences: Boolean,
+    playSound: Boolean,
+  },
+  data() {
+    const schedule = new Schedule(this.durationWork, this.durationBreak, this.durationLongBreak)
+    return {
+      intervalId: undefined,
+      startDate: undefined,
+      schedule: schedule,
+      display: this.formatTimeSpan(schedule.currentStep().duration),
     }
+  },
+  methods: {
+    toggleTimer: function () {
+      if (this.intervalId) {
+        this.stopTimer()
+      } else {
+        this.startTimer()
+      }
+    },
+    startTimer: function () {
+      this.startDate = new Date()
+
+      this.intervalId = setInterval(() => {
+        const leftSeconds = this.secondsLeftInStep()
+        this.setTimerDisplay(this.formatTimeSpan(leftSeconds))
+
+        if (leftSeconds <= 0) {
+          this.schedule.moveToNextStep()
+          this.stopTimer()
+          this.notify()
+        }
+      }, 100)
+    },
+    stopTimer: function () {
+      clearInterval(this.intervalId)
+      this.intervalId = null
+      this.setTimerDisplay(this.formatTimeSpan(this.schedule.currentStep().duration))
+      document.title = "Pomotauri"
+    },
+    goToNextStep: function () {
+      this.schedule.moveToNextStep()
+      this.stopTimer()
+    },
+    notify: function () {
+      if (this.playSound) {
+        let audio = document.getElementById('beepAudio');
+        audio.play()
+      }
+    },
+    secondsLeftInStep: function () {
+      const diffSeconds = (new Date() - this.startDate) / 1000
+      return this.schedule.currentStep().duration - diffSeconds
+    },
+    formatTimeSpan: function (spanSeconds) {
+      const minutes = Math.floor(spanSeconds / 60)
+      const seconds = Math.floor(spanSeconds - minutes * 60)
+      return this.doubleDigit(minutes) + ":" + this.doubleDigit(seconds)
+    },
+    doubleDigit: function (number) {
+      return (number).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false})
+    },
+    setTimerDisplay: function (text) {
+      this.display = text
+      document.title = text
+    },
+    togglePreferences: function () {
+      this.stopTimer()
+      this.$emit('update:displayPreferences', !this.displayPreferences)
+    }
+  }
 }
 </script>
 
 <style>
 #timer {
-    font-size: 2.5rem;
-    font-weight: bold;
-    margin-bottom: 25px;
+  font-size: 3.5rem;
+  font-weight: bold;
+  margin-bottom: 25px;
+  line-height: 1;
 }
 
-.btn-timer {
-    margin: 0 5px;
-    padding: 8px 15px;
-    background-color: #89c3ef;
-    border: none;
-    border-radius: 5px;
-    font-size: 1.2rem;
-    color: #f0ffff;
-}
 
-.btn-timer:hover {
-    background-color: #004c99;
-    cursor: pointer;
-}
 </style>
